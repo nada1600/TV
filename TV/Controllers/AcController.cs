ba749f9c263255701140feeeb4be005cf7613ca2
nada1600 <nada1600@student.miun.se>
2018-01-24 21:42:19 +0100

    Lagt till en ny AccountController *AcController och skapat en ViewModel

---
+++ TV/Controllers/AcController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TV.Controllers
+{
+    public class AcController : Controller
+    {
+        // GET: Ac
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ TV/Controllers/ProgramsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TV.Models.db;
+
+namespace TV.Controllers
+{
+    public class ProgramsController : Controller
+    {
+        private testDBEntities db = new testDBEntities();
+
+        // GET: Programs
+        public ActionResult Index()
+        {
+            var program = db.Program.Include(p => p.Category1).Include(p => p.Chanel1);
+            return View(program.ToList());
+        }
+
+        // GET: Programs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Program program = db.Program.Find(id);
+            if (program == null)
+            {
+                return HttpNotFound();
+            }
+            return View(program);
+        }
+
+        // GET: Programs/Create
+        public ActionResult Create()
+        {
+            ViewBag.Category = new SelectList(db.Category, "Id", "Name");
+            ViewBag.Chanel = new SelectList(db.Chanel, "Id", "Name");
+            return View();
+        }
+
+        // POST: Programs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Titel,Description,Chanel,Category,Starttime,Endtime")] Program program)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Program.Add(program);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Category = new SelectList(db.Category, "Id", "Name", program.Category);
+            ViewBag.Chanel = new SelectList(db.Chanel, "Id", "Name", program.Chanel);
+            return View(program);
+        }
+
+        // GET: Programs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Program program = db.Program.Find(id);
+            if (program == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Category = new SelectList(db.Category, "Id", "Name", program.Category);
+            ViewBag.Chanel = new SelectList(db.Chanel, "Id", "Name", program.Chanel);
+            return View(program);
+        }
+
+        // POST: Programs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Titel,Description,Chanel,Category,Starttime,Endtime")] Program program)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(program).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Category = new SelectList(db.Category, "Id", "Name", program.Category);
+            ViewBag.Chanel = new SelectList(db.Chanel, "Id", "Name", program.Chanel);
+            return View(program);
+        }
+
+        // GET: Programs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Program program = db.Program.Find(id);
+            if (program == null)
+            {
+                return HttpNotFound();
+            }
+            return View(program);
+        }
+
+        // POST: Programs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Program program = db.Program.Find(id);
+            db.Program.Remove(program);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
