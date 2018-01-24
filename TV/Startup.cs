2a24f8eaba644928759e8e9713ebba3f595de44f
nada1600 <nada1600@student.miun.se>
2018-01-24 04:38:12 +0100

    (: ***PROJEKT-START*** :)

---
+++ TV/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TV.Startup))]
+namespace TV
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
