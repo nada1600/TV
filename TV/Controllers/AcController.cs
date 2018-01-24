748ae025f1c202011b3de328974638ac4e6491a2
nada1600 <nada1600@student.miun.se>
2018-01-24 23:33:33 +0100

    Lagt till funktion för cookie

--- TV/Controllers/AcController.cs
+++ TV/Controllers/AcController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using TV.Models.ViewModels;
 
 namespace TV.Controllers
@@ -15,12 +16,14 @@ namespace TV.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult Login(LoginVM model)
+        public ActionResult Login(LoginVM model, string ReturnUrl)
         {
             //Kontrollerar om alla fält har fyllts i
             if (ModelState.IsValid)
             {
-
+                //Skapar en cookie som inte är persistent. Den försvinner när browsern stängs ner.
+                FormsAuthentication.SetAuthCookie(model.Username, false);
+                return Redirect(ReturnUrl);
             }
             return View();
         }
