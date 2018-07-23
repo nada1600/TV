bad59cde71eaa714cdfe07b7ebbd0a921051ae16
nada1600 <nada1600@student.miun.se>
2018-07-23 08:50:05 +0200

    Lagt till "Hem"

--- TV/App_Start/RouteConfig.cs
+++ TV/App_Start/RouteConfig.cs
@@ -14,6 +14,14 @@ namespace TV
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Hem",
+                url: "Hem/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+
+            //Dont touch!
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
